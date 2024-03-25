e57fcbd91db01e076d78894742ecda8e85a32e03
sunnyAonep <Arbiv.sunny@gmail.com>
2024-03-25 21:01:21 +0200

    chat

--- Client/src/components/chat/chatOnline.jsx
+++ Client/src/components/chat/chatOnline.jsx
@@ -1,26 +1,59 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import axios from 'axios';
 import logo from '../../assets/img/Anonimos.png'
 import { APIBaseUrl } from '../../config/baseUrl';
-// import "chatOnline.css"
+import "./chatOnline.css"
+import { UserContext } from '../../managers/userManager';
 export default function ChatOnline({onlineUsers, currentId , setCurrentChat}) {
+  const { user } = useContext(UserContext)
   const [Psyco , setPsyco] = useState([]);
-  const [onlineFriends , setOnlineFriends] = useState([])
-  useEffect(()=>{
-    const getPsyco = async()=>{
-      const res = await axios.get(`${APIBaseUrl}/users/find`)
-      setPsyco(res.data.users)
-    }
-    getPsyco()
-  },[currentId])
-  console.log(Psyco);
+  const [therapist , setTherapist] = useState([]);
+  const [onlineUser , setOnlineUsers] = useState([])
+  const [onlineTherapist , setOnlineTherapist] = useState([])
+
+  useEffect(() => {
+    const getPsycoUsers = async () => {
+      try {
+        const res = await axios.get(`${APIBaseUrl}/users/find?role=user`);
+        // Assuming res.data.users is an array of user objects
+        const filteredUsers = res.data.users.filter((username) => username._id !== user.id);
+        setPsyco(filteredUsers);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    getPsycoUsers();
+  }, [APIBaseUrl, user.id, currentId]);
+
+  useEffect(() => {
+    const getTherapist = async () => {
+      try {
+        const res = await axios.get(`${APIBaseUrl}/users/find?role=therapist`);
+        // Assuming res.data.users is an array of user objects
+        const filteredUsers = res.data.users.filter((username) => username._id !== user.id);
+        setTherapist(filteredUsers);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    getTherapist();
+  }, [APIBaseUrl, user.id, currentId]);
+  
+  useEffect(() => {
+    const filteredFriends = therapist.filter(Psyco => {
+      return onlineTherapist.some(onlineTherapist => onlineTherapist.userId === Psyco._id);
+    });
+    setOnlineUsers(filteredFriends);
+  }, [Psyco, onlineTherapist]);
+
   useEffect(() => {
     const filteredFriends = Psyco.filter(Psyco => {
       return onlineUsers.some(onlineUser => onlineUser.userId === Psyco._id);
     });
-    setOnlineFriends(filteredFriends);
+    setOnlineUsers(filteredFriends);
   }, [Psyco, onlineUsers]);
-
   const handleClick = async(user)=>{
     try{
       const res = await axios.get(`${APIBaseUrl}/room/find/${user._id}`)
@@ -36,10 +69,10 @@ export default function ChatOnline({onlineUsers, currentId , setCurrentChat}) {
       console.log(err);
     }
   }
-  console.log(onlineFriends);
+  console.log(onlineUser);
   return (
     <div className='chatOnline'>
-      {onlineFriends.map((o)=>(
+      {onlineUser.map((o)=>(
       <div className="chatOnlineFriends" onClick={()=>handleClick(o)}>
         <div className="chatOnlineImgContainer">
             <img src={o?.profileImg? o.profileImg :logo} alt="" className='chatOnlineImg'/>
@@ -49,6 +82,7 @@ export default function ChatOnline({onlineUsers, currentId , setCurrentChat}) {
         <span  className="chatOnlineName">{o.user_name}</span>
       </div>
       ))}
+      
     </div>
   )
 }
--- Server/controllers/user.controller.js
+++ Server/controllers/user.controller.js
@@ -101,4 +101,17 @@ const updateUser = async(req, res) => {
     }
 };
 
-module.exports = { register , login, deleteUser, updateUser, getUsers, showUser}
+const getUsersinQuary = async (req, res) => {
+    const userId = req.query.userId;
+    const username = req.query.username;
+    try {
+      const user = userId
+        ? await User.findById(userId)
+        : await User.findOne({ username: username });
+      const { password, updatedAt, ...other } = user._doc;
+      res.status(200).json(other);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  };
+module.exports = { register , login, deleteUser, updateUser, getUsers, showUser , getUsersinQuary}
