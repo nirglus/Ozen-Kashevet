eb2d975b1629bf21836b9c36e1f51bab13afa369
No2Cookies <danzemly000@gmail.com>
2024-03-25 20:07:56 +0200

    log out linked to navbar

--- Client/src/components/navbar/navbar.jsx
+++ Client/src/components/navbar/navbar.jsx
@@ -1,51 +1,40 @@
-import { useState } from "react"
-import { NavLink } from "react-router-dom"
+import { useContext } from "react";
+import { NavLink } from "react-router-dom";
 import 'animate.css';
+import { UserContext } from "../../managers/userManager";
 
 export default function Navbar(props) {
+    const { logOut, user } = useContext(UserContext);
 
     const collapseHandler = () => {
-        props.setIsCollapsed(!props.isCollapsed)
+        props.setIsCollapsed(!props.isCollapsed);
+    };
 
-    }
-
-    if (props.isCollapsed === false) {
-
-        return (<nav className="animate__animated animate__slideInLeft animate__faster w-1/5 h-screen fixed bg-red-500 z-50">
-            <button onClick={collapseHandler}>close sidebar</button>
-            <ul>
-                <NavLink to='/auth'>
-                    <li>Auth</li>
-                </NavLink>
-                <NavLink to='/chat'>
-                    <li>Message</li>
-                </NavLink>
-                <NavLink to='/dashboard'>
-                    <li>Dashboard</li>
-                </NavLink>
-            </ul>
-
-        </nav>)
-    }
-
-    return (<div>
-        <button className="fixed" onClick={collapseHandler}>open sidebar</button>
-        <nav className="animate__animated animate__slideOutLeft animate__faster w-1/5 h-screen fixed bg-red-500 z-50  ">
-            <button onClick={collapseHandler}>close sidebar</button>
-            <ul>
-                <NavLink to='/auth'>
-                    <li>Auth</li>
-                </NavLink>
-                <NavLink to='/chat'>
-                    <li>Message</li>
-                </NavLink>
-            </ul>
-
-        </nav>
-
-    </div>
-    )
-
-
+    return (
+        <div>
+            <button className="fixed" onClick={collapseHandler}>
+                {props.isCollapsed ? 'open sidebar' : 'close sidebar'}
+            </button>
+            <nav className={`animate__animated animate__faster w-1/5 h-screen fixed bg-red-500 z-50 ${props.isCollapsed ? 'animate__slideOutLeft' : 'animate__slideInLeft'
+                }`}>
+                <button onClick={collapseHandler}>close sidebar</button>
+                <ul>
+                    <NavLink to='/auth'>
+                        <li>Auth</li>
+                    </NavLink>
+                    <NavLink to='/chat'>
+                        <li>Message</li>
+                    </NavLink>
+                    {user && Object.keys(user).length > 0 && (
+                        <>
+                            <NavLink to='/dashboard'>
+                                <li>Dashboard</li>
+                            </NavLink>
+                            <button onClick={logOut}>Log Out</button>
+                        </>
+                    )}
+                </ul>
+            </nav>
+        </div>
+    );
 }
-
