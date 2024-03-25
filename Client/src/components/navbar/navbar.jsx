de67e7ec4338dfdb470f03224238338d5d15a12e
No2Cookies <danzemly000@gmail.com>
2024-03-25 20:28:42 +0200

    icons

--- Client/src/components/navbar/navbar.jsx
+++ Client/src/components/navbar/navbar.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import 'animate.css';
 import { UserContext } from "../../managers/userManager";
+import { GoSidebarExpand, GoSidebarCollapse } from 'react-icons/go'
 
 export default function Navbar(props) {
     const { logOut, user } = useContext(UserContext);
@@ -12,25 +13,25 @@ export default function Navbar(props) {
 
     return (
         <div>
-            <button className="fixed" onClick={collapseHandler}>
-                {props.isCollapsed ? 'open sidebar' : 'close sidebar'}
+            <button className="fixed text-3xl" onClick={collapseHandler}>
+                <GoSidebarCollapse />
             </button>
             <nav className={`animate__animated animate__faster w-1/5 h-screen fixed bg-red-500 z-50 ${props.isCollapsed ? 'animate__slideOutLeft' : 'animate__slideInLeft'
                 }`}>
-                <button onClick={collapseHandler}>close sidebar</button>
+                <button className="flex justify-end w-full text-3xl pr-2" onClick={collapseHandler}><GoSidebarExpand /></button>
                 <ul>
                     <NavLink to='/auth'>
-                        <li>Auth</li>
+                        <li className="flex justify-center">Auth</li>
                     </NavLink>
                     <NavLink to='/chat'>
-                        <li>Message</li>
+                        <li className="flex justify-center">Message</li>
                     </NavLink>
                     {user && Object.keys(user).length > 0 && (
                         <>
                             <NavLink to='/dashboard'>
-                                <li>Dashboard</li>
+                                <li className="flex justify-center">Dashboard</li>
                             </NavLink>
-                            <button onClick={logOut}>Log Out</button>
+                            <button className="flex justify-center w-full mt-auto" onClick={logOut}>Log Out</button>
                         </>
                     )}
                 </ul>
