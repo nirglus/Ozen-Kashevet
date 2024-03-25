9651b4d55590c46978e8fb21f78611a5979e7c87
No2Cookies <danzemly000@gmail.com>
2024-03-25 11:41:36 +0200

    rfc

---
+++ Client/src/assets/components/chat/chatCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function chatCard() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ Client/src/assets/components/chat/pr.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function pr() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
