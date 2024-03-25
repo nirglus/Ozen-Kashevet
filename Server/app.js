a6997bdb77bdb0a124315b6aafe6e45aed000e5e
sunnyAonep <Arbiv.sunny@gmail.com>
2024-03-25 14:57:36 +0200

    messages

--- Server/app.js
+++ Server/app.js
@@ -3,6 +3,7 @@ const messages = require("./routes/message.routes");
 const room = require("./routes/room.routes")
 const user = require("./routes/user.routes");
 const meeting = require("./routes/meeting.routes");
+
 const cors = require("cors")
 const app = express();
 app.use(cors())
@@ -10,8 +11,8 @@ app.use(express.json())
 
 app.use("/api/v1/messages" , messages);
 app.use("/api/v1/room" , room);
-app.use("api/v1/users", user);
-app.use("api/v1/meetings", meeting);
+app.use("/api/v1/users", user);
+app.use("/api/v1/meetings", meeting);
 
 
 
