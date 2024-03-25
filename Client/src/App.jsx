6970354898923d0b63bf0dca7c886f4cd64e2058
No2Cookies <danzemly000@gmail.com>
2024-03-25 11:59:28 +0200

    routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ChatPage from './pages/chat/chatPage'
+import Auth from './pages/auth/auth'
 
 function App() {
 
   return (
-    <main>
-    </main>
+    <BrowserRouter>
+      {/* <UserManager>
+        <Navbar></Navbar> */}
+      <Routes>
+        <Route path='/chat' element={<ChatPage />}></Route>
+        <Route path='/auth' element={<Auth />}></Route>
+      </Routes>
+
+      {/* </UserManager> */}
+    </BrowserRouter>
   )
 }
 
