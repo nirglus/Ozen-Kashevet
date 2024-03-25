da540393a39f28de9d4fab4728f9aebf1f4e6ab1
No2Cookies <danzemly000@gmail.com>
2024-03-25 19:49:44 +0200

    dashboard

--- Client/src/pages/dashboard/dashboard.jsx
+++ Client/src/pages/dashboard/dashboard.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { NavLink } from "react-router-dom"
 
 export default function Dashboard() {
     const [popUp, setPopUp] = useState('hidden')
@@ -9,12 +10,12 @@ export default function Dashboard() {
             <section id="menu" className="flex flex-wrap justify-center w-full h-[30vh] items-center">
                 <h1 className="w-full font-bold font-size flex justify-center">DASHBOARD</h1>
                 <div className="flex w-full justify-center gap-3">
-                    <button className="menuButton">Chat with a proffesional</button>
+                    <NavLink to='/chat'><button className="menuButton">Chat with a proffesional</button></NavLink>
                     <button className="menuButton">AI Chatbot</button>
                     <button className="menuButton">My Meetings</button>
                 </div>
                 <div className="flex w-full justify-center gap-3">
-                    <button className="menuButton">My Chats</button>
+                    <NavLink to='/chat'><button className="menuButton">My Chats</button></NavLink>
                     <button className="menuButton">Read Helpful articles</button>
                     <button className="menuButton">PLACEHOLDER</button>
                 </div>
