d669c849440630e05ea0a739db954f23895c085d
nirglus <nirgluskin@gmail.com>
2024-03-26 09:36:51 +0200

    disable therapistShcedule

--- Server/models/therapistSchedule.model.js
+++ Server/models/therapistSchedule.model.js
@@ -1,10 +1,10 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const therapistScheduleSchema = new mongoose.Schema({
-    meetings: [{type: mongoose.Schema.Types.ObjectId, ref: 'Meeting'}],
-    therapist_id: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
-}, {timestamps: true})
+// const therapistScheduleSchema = new mongoose.Schema({
+//     meetings: [{type: mongoose.Schema.Types.ObjectId, ref: 'Meeting'}],
+//     therapist_id: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+// }, {timestamps: true})
 
-const TherapistSchedule = mongoose.model("TherapistSchedule", therapistScheduleSchema);
+// const TherapistSchedule = mongoose.model("TherapistSchedule", therapistScheduleSchema);
 
-module.exports = { TherapistSchedule };
+// module.exports = { TherapistSchedule };
--- Server/routes/therapistSchedule.routes.js
+++ Server/routes/therapistSchedule.routes.js
@@ -1,9 +1,9 @@
-const { Router } = require("express");
-const { createTherapistSchedule, getTherapistSchedule, updateTherapistSchedule } = require("../controllers/therapistSchedule.controller");
-const router = Router();
+// const { Router } = require("express");
+// const { createTherapistSchedule, getTherapistSchedule, updateTherapistSchedule } = require("../controllers/therapistSchedule.controller");
+// const router = Router();
 
-router.post("/", createTherapistSchedule);
-router.get("/:id", getTherapistSchedule);
-router.patch("/:id", updateTherapistSchedule);
+// router.post("/", createTherapistSchedule);
+// router.get("/:id", getTherapistSchedule);
+// router.patch("/:id", updateTherapistSchedule);
 
-module.exports = router;
+// module.exports = router;
