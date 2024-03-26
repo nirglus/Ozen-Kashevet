c5e3418ed9225676565ab52a315546b7e4e7f399
yizak223 <139069183+yizak223@users.noreply.github.com>
2024-03-26 15:13:04 +0200

    commit

--- Client/src/pages/dashboard/dashboard.jsx
+++ Client/src/pages/dashboard/dashboard.jsx
@@ -56,6 +56,19 @@ export default function Dashboard() {
         setAIModalOpen(false);
     };
 
+
+    function formatDateAndTime(dateString) {
+        const date = new Date(dateString);
+        const year = date.getFullYear();
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // Month starts from 0
+        const hour = String(date.getHours()).padStart(2, '0');
+        const minute = String(date.getMinutes()).padStart(2, '0');
+
+        return `${day}/${month}/${year} ${hour}:${minute}`;
+    }
+
+
     useEffect(() => {
         const fetchMeetings = async () => {
             try {
@@ -112,10 +125,12 @@ export default function Dashboard() {
                     </div>
                     <button className="text-3xl" onClick={closeMeetingModal}><IoMdClose className="hover:text-red-600" /></button>
                 </div>
-                {meetingsData.map((meeting) => {
+
+                {meetingsData.map( (meeting) => {
                     return (
-                        <div key={meeting.id}>
-                            {meeting.date}
+                        <div className="bg-gray-100 p-4 rounded-lg" key={meeting.id}>
+                            {formatDateAndTime(meeting.date)}
+
                         </div>
                     );
                 })}
--- Client/src/pages/dashboard/modal.jsx
+++ Client/src/pages/dashboard/modal.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Modal = ({ isOpen, onclose , children }) => {
     if (!isOpen) return null;
-
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
             <div className="bg-white p-6 rounded-lg shadow-lg h-[80vh] w-[50vw] flex  flex-wrap justify-between flex-col">
