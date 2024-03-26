581ec57b9a4f82349deaecb94be307011d571d13
No2Cookies <danzemly000@gmail.com>
2024-03-26 09:35:00 +0200

    dashmodal

--- Client/src/pages/dashboard/dashboard.jsx
+++ Client/src/pages/dashboard/dashboard.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Modal from "./modal"; // Assuming you have a Modal component
 import { IoChatbubble } from "react-icons/io5";
-
+import { IoMdClose } from "react-icons/io";
 
 export default function Dashboard() {
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -19,7 +19,7 @@ export default function Dashboard() {
         <div className="w-full h-[95vh] items-center flex flex-wrap justify-around">
             <section id="menu" className="flex flex-wrap justify-center w-full h-[30vh] items-center">
                 <h1 className="w-full font-bold font-size flex justify-center">DASHBOARD</h1>
-                <div className="flex w-full justify-center gap-3">
+                <div className="flex w-full justify-center gap-3 mb-6">
                     <NavLink to='/chat'><button className="menuButton">Chat with a professional</button></NavLink>
                     <button className="menuButton" onClick={openModal}>AI Chatbot</button>
                     <button className="menuButton">My Meetings</button>
@@ -31,14 +31,20 @@ export default function Dashboard() {
                 </div>
             </section>
             <Modal isOpen={isModalOpen} onClose={closeModal}>
-                <div className="flex items-center">
-                    <IoChatbubble className="mr-2 text-xl" />
-                    <h2 className="text-lg font-semibold">Chatbot</h2>
+                <div className="flex items-center justify-between">
+                    <div className="flex items-center">
+                        <IoChatbubble className="mr-2 text-xl" />
+                        <h2 className="text-lg font-semibold">Chatbot</h2>
+                    </div>
+                    <button className="text-3xl" onClick={closeModal}><IoMdClose className="hover:text-red-600" /></button>
                 </div>
-                <div className="h-[50%] w-full border border-black"></div>
+
+                <div className="h-[50%] w-full border border-black">
+
+                </div>
                 <div>
                     <input className="w-[90%] h-10 border border-black p-2" placeholder="Type Your Message:"></input>
-                    <button className="bg-red-500 p-2 border border-black" onClick={closeModal}>close</button>
+                    <button className="bg-green-500 p-2 border border-black">Send</button>
                 </div>
             </Modal>
         </div>
