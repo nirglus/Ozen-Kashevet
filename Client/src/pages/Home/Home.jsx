cae3b28e1a7cc773096294d0cef0f7e9667cf34f
nirglus <nirgluskin@gmail.com>
2024-03-30 08:20:22 +0300

    adjusting package.json, add Home page

---
+++ Client/src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <section className="l-side">
+        <div className="headings">
+            <h1>Finding balance <span>In Mental</span> <span>Wellness.</span></h1>
+        </div>
+        <div className="texts flex">
+            
+        </div>
+      </section>
+      <section className="r-side">
+
+      </section>
+    </div>
+  )
+}
+
+export default Home
