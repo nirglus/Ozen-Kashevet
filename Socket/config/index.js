5152f791a585778b1ddd0e05ef0b3b548baddb01
nirglus <nirgluskin@gmail.com>
2024-06-17 20:59:09 +0300

    add dot env for socket

--- Socket/config/index.js
+++ Socket/config/index.js
@@ -1,7 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const io = require("socket.io")(8900, {
     cors:{
-        origin:"http://localhost:5173"
+        origin: process.env.SOCKET_IO_ORIGIN
     }
 });
-
 module.exports = {io}
